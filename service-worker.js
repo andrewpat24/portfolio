9b72b3c5d571ecd28d47e2abd4968eb898839c83
Andrew Patterson <apat24@mail.sfsu.edu>
2019-06-21 17:24:59 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3a238cf1ea59259372dee6a257390c78.js"
+  "/portfolio/precache-manifest.06aaf84f209ca3ba0731e52a15a54407.js"
 );
 
 self.addEventListener('message', (event) => {
